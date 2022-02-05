be29572434a10eaba6bb216085e63b0bff2b3fde
hongbi <khb1620@naver.com>
2022-02-05 23:48:07 +0900

    20220205

---
+++ 이것이 코딩테스트다/04-1. 구현_예제.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  5 23:40:55 2022
+
+@author: khb16
+"""
+
+# 4-1. 상하좌우
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+
+# L, R, U, D에 따른 이동 방향
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+move_types = ['L','R','U','D']
+
+# 이동 계획을 하나씩 확인
+for plan in plans:
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    # 공간을 벗어나는 경우 무시
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    # 이동 수행
+    x, y = nx, ny
+print(x, y)
+# 5
+
+# R R R U D D
+# 3 4
+
+
+# 4-2. 시각
+h = int(input())
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+print(count)
+# 5
+# 11475
