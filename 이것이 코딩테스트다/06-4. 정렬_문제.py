eaff921d7e7088d71800f87bc87d09e17cc58df4
hongbi <khb1620@naver.com>
2022-02-17 23:46:53 +0900

    20220217

--- 이것이 코딩테스트다/06-4. 정렬_문제.py
+++ 이것이 코딩테스트다/06-4. 정렬_문제.py
@@ -5,6 +5,31 @@ Created on Sun Jan 16 01:38:18 2022
 @author: khb16
 """
 
+# 문제3
+n = int(input())
+
+array = []
+for i in range(n):
+    input_data = input().split()
+    # 이름은 문자열 그대로, 정수는 정수형으로 변환하여 저장
+    array.append((input_data[0], int(input_data[1])))
+
+# 키(Key)를 이용하여, 점수를 기준으로 정렬
+array = sorted(array, key = lambda student: student[1])
+
+# 정렬이 수행된 결과를 출력
+for student in array:
+    print(student[0], end = ' ')
+    
+'''
+2
+
+홍길동 95
+
+이순신 77
+이순신 홍길동
+'''    
+
 # 문제
 # 4. 두 배열의 원소 교체
 # 두 개의 배열 A와 B는 N개의 원소로 구성, 모두 자연수
