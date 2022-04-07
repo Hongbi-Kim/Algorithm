db5306d5df840083f4c35c34b0de078c19dbd4d3
hongbi <khb1620@naver.com>
2022-04-08 00:54:47 +0900

    20220408

--- 이것이 코딩테스트다/11-1. 그리디.py
+++ 이것이 코딩테스트다/11-1. 그리디.py
@@ -111,10 +111,69 @@ print(target)
 8
 '''
 
+# 5
+# 볼링공 고르기
 
+n , m = map(int, input().split())
+data = list(map(int, input().split()))
 
+# 1부터 10까지의 무게를 담을 수 있는 리스트
+array = [0] * 11
 
+for x in data:
+    # 각 무게에 해당하는 볼링공의 개수 카운트
+    array[x] += 1
+    
+result = 0
+# 1부터 m까지의 각 무게에 대하여 처리
+for i in range(1, m+1):
+    n -= array[i] # 무게가 i인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
+    result += array[i] * n # B가 선택하는 경우의 수와 곱하기
+print(result)
 
+'''
+5 3
+
+1 3 2 3 2
+8
+
+8 5
+
+1 5 4 3 2 4 5 2
+25
+'''
+
+# 6. 무지의 먹방 라이브
+# 카카오 신입 공채
+
+import heapq
+
+def solution(food_times, k):
+    # 전체 음식을 먹는 시간보다 k가 크다면 -1
+    if sum(food_times) <= k:
+        return -1
+    
+    # 시간이 작은 음식부터 빼야하므로 우선순위 큐를 이용
+    q = []
+    for i in range(len(food_times)):
+        # (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
+        heapq.heapush(q, (food_times[i], i+1))
+        
+    sum_value = 0 # 먹기 위해 사용한 시간
+    previous = 0 # 직전에 다 먹은 음식 시간
+    length = len(foom_times) # 남은 음식의 개수
+    
+    # sum_value + (현재의 음식 시간 - 이전의 음식 시간) * 현재 음식 개수 k와 비교
+    while sum_value + ((q[0][0] - previous) * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1 # 다 먹은 음식 제외
+        previous = now # 이전 음식 시간 재설정
+        
+    # 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
+    result = sorted(1, key = lambda x:x[1]) # 음식의 번호 기준으로 정렬
+    return result[(k-sum_value) % length][1]
+        
 
 
 
