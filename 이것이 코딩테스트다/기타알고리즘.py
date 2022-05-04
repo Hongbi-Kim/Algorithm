f897c1e584fafcf5661fe9d490dfd16c649c00cd
hongbi <khb1620@naver.com>
2022-05-04 23:24:47 +0900

    20220504

--- 이것이 코딩테스트다/기타알고리즘.py
+++ 이것이 코딩테스트다/기타알고리즘.py
@@ -35,5 +35,78 @@ for a, b in zip(words, words[1:] + ['']):
         cut_word = b
         cur_cnt = 1
 sum(len(word) + (len(str(cnt)) if cnt >1 else 0) for word, cnt in res)
-    
 
+# --------------
+number = "1231234"
+k = 3
+L=[]
+for i in number:
+    L.append(i)
+L.sort(reverse = True)
+answer = ""
+for i in L[0:len(number)-k]:
+    answer += "".join(i)
+def solution(number, k):
+    L=[]
+    for i in number:
+        L.append(i)
+    L.sort(reverse = True)
+    answer = ""
+    for i in L[0:len(number)-k]:
+        answer += "".join(i)
+    return answer
+    
+solution("1231234",3)
+
+"".join([123])
+
+
+L=[]
+for n in number:
+    while L and L[-1] < n and k>0:
+        L.pop()
+        k -= 1
+    L.append(n)
+while k > 0:
+    L.pop()
+    k -= 0
+answer = "".join(L)
+L.sort(reverse = True)
+
+### -------
+import re
+new_id = "...!@BaT#*..y.abcdefghijklm"
+new_id = new_id.lower()
+new_id = re.sub(r"[^a-z0-9\-\_\.]","",new_id)
+new_id = new_id.replace("..",".")
+if new_id == "":
+    new_id = "a"
+elif len(new_id) >= 16:
+    new_id = new_id[:15]
+    if new_id[-1] == ".":
+        new_id = new_id[:14]
+if len(new_id) <= 2:
+    while len(new_id) == 3:
+        new_id += new_id[-1]
+new_id + new_id[-1]
+"bat.y.abcdefghi"
+
+import re
+
+def solution(new_id):
+    st = new_id
+    st = st.lower()
+    st = re.sub('[^a-z0-9\-_.]', '', st)
+    st = re.sub('\.+', '.', st)
+    st = re.sub('^[.]|[.]$', '', st)
+    st = 'a' if len(st) == 0 else st[:15]
+    st = re.sub('^[.]|[.]$', '', st)
+    st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
+    return st
+
+solution("...!@BaT#*..y.abcdefghijklm")
+
+numbers = [6, 10, 2]
+numbers = [str(x) for x in numbers]
+numbers.sort(key = lambda x : (x*4)[:4], reverse = True)
+''.join(numbers)
