f6f5ae158f7e8cdc8ba912777beb5ee58c942103
hongbi <khb1620@naver.com>
2022-06-10 22:26:27 +0900

    20220610

---
+++ 04. 해시(Key-value)/카카오_lv3_추석트래픽.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  8 17:55:05 2022
+
+@author: khb16
+"""
+# 문제 : https://programmers.co.kr/learn/courses/30/lessons/17676
+# 문제 해설 : https://tech.kakao.com/2017/09/27/kakao-blind-recruitment-round-1/
+
