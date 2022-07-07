1339ca5e5c25e89ab1dd3d7de8117e03361ffc25
hongbi <khb1620@naver.com>
2022-07-07 22:46:56 +0900

    20220707

---
+++ 프로그래머스/[DFS_BFS] lv3. 네트워크.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 22:43:53 2022
+
+@author: khb16
+"""
+
+# DFS/BFS
+# 네트워크
+
+computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+n = 3
+
+def dfs(x, computers, visited):
+    visited[x] = True    # x노드 방문 처리
+    for a, b in enumerate(computers[x]):
+        if b == 1 and (not visited[a]):    # 연결되어 있고, 아직 방문하지 않았다면
+            dfs(a, computers, visited)
+
+def solution(n, computers):
+    visited = [False] * n    # 방문기록 초기화
+    cnt = 0    # 그룹 개수
+    
+    for i in range(n):
+        if not visited[i]:
+            dfs(i, computers, visited)
+            cnt += 1
+            
+    return cnt
+
+# 2
